24f75eb9e15a657e39ffeae78f1c7e21771b878b
inpechman <inpechman@gmail.com>
2019-03-09 21:00:17 +0200

    initial commit

---
+++ app/src/main/java/com/sruly/stu/toilets/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sruly.stu.toilets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
